10b025a1b6153375ea1d1fdddd25a89cd1cf772a
koryunskr-svg <koryun.skr@gmail.com>
2025-09-09 20:48:21 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+import os
+import pytz
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
+CALENDAR_ID = os.getenv("CALENDAR_ID")
+SHEET_ID = os.getenv("SHEET_ID")
+TIMEZONE = pytz.timezone(os.getenv("TZ", "Europe/Moscow"))
+RESERVATION_TIMEOUT = 120
+WARNING_TIMEOUT = 60
