6defc7fac5fec929f3efa27210fd6d2e91ff0610
koryunskr-svg <koryun.skr@gmail.com>
2025-10-22 21:53:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-# config.py
 import os
+from dotenv import load_dotenv
 import pytz
+
+load_dotenv()  # ← ЭТА СТРОКА ОБЯЗАТЕЛЬНА
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
@@ -9,4 +11,3 @@ SHEET_ID = os.getenv("SHEET_ID")
 TIMEZONE = pytz.timezone(os.getenv("TZ", "Europe/Moscow"))
 RESERVATION_TIMEOUT = 120
 WARNING_TIMEOUT = 60
-
